69f81ec457098c887c5b3ac8973fad66ddf7906e
Rv Brindle <rv.brindle@gmail.com>
2025-01-13 22:05:57 +0000

    test

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,7 +1,7 @@
 import {COMPANY_NAME} from '../consts/consts.js'
 export default function Logo({small= false}) {
 
-    const logo = ((small === true)) ? '/Logo-small.png' : '/Logo.png'
+    const logo = ((small === true)) ? './Logo-small.png' : './Logo.png'
 
     return (
         <div className='w-8-rem h-4-rem'>
